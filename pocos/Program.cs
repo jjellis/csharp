81e2f34ffffbb1f528ff4c20f7eac5bce3d6277d
jjn42 <jjrr1234@aol.com>
2019-06-04 18:17:08 -0500

    ..

--- pocos/Program.cs
+++ pocos/Program.cs
@@ -10,6 +10,37 @@ namespace pocos
     {
         static void Main(string[] args)
         {
+            
+
+            Console.WriteLine("what is your first name?");
+             string UserFirstName = Console.ReadLine().ToLower();
+
+            Console.WriteLine("what is your last name?");            
+             string UserLastName = Console.ReadLine().ToLower();
+
+            Console.WriteLine("what is your gender?");
+            string UserGender = Console.ReadLine().ToLower();
+
+            Console.WriteLine("what is your license number?");            
+            string UserLNumber = Console.ReadLine().ToLower();
+
+
+            dl FirstLastName = new dl();
+            FirstLastName.FirstName = UserFirstName;
+            FirstLastName.LastName = UserLastName;
+            Console.WriteLine("{0} {1} is your full name",
+                 FirstLastName.FirstName, FirstLastName.LastName 
+                );
+            Console.ReadLine();
         }
+
     }
+    public class dl
+    {
+        public string FirstName;
+        public string LastName;
+        public string Gender;
+        public string LNumber;
+    }
+
 }
--- pocos/Properties/AssemblyInfo.cs
+++ pocos/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("pocos")]
+[assembly: AssemblyTitle("PoCos")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("pocos")]
+[assembly: AssemblyProduct("PoCos")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("35dc9ebc-995d-4f64-8102-42e2b364ea3e")]
+[assembly: Guid("bd10f864-b3c6-4414-92a4-db3e6eb2fbb2")]
 
 // Version information for an assembly consists of the following four values:
 //
