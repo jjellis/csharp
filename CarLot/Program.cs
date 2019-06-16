8567e12f1c79f2fa19c5807cecf0f0ef88d9fea5
jjn42 <jjrr1234@aol.com>
2019-06-15 20:23:42 -0500

    cars

--- CarLot/Program.cs
+++ CarLot/Program.cs
@@ -10,10 +10,108 @@ namespace CarLot
     {
         static void Main(string[] args)
         {
+
+            List<Vehicle> invetory = new List<Vehicle>();
+            CarLot pioner = new CarLot("pioner", invetory);
+            CarLot fordcars = new CarLot("fordcars", invetory); 
+            .Add(new Car("tat999", "honda", "liberty", 6000, "coupe", 4));
+            invetory.Add(new Truck("dbd8943", "Ford", "F-150", 20000, "short"));
+            invetory.Add(new Car("FCH938", "hyundai", "sonata", 10000, "Hatchback", 4));            
+            pioner.list(new Car("DBG8D966", "Chevoltet", "Tracker", 9999.99, "Sedan", 4));
+            pioner.list(new Truck("DBG8D967", "Ford", "F-150", 1000.01, "Medium"));
+            pioner.list(new Car("DBG8D968", "Toyota", "Camrie", 5.01, "Hatchback", 1));
+            fordcars.list(new Truck("pne0032", "Ford", "F-250", 20000, "long"))
+            fordcars.Print();
+            Console.WriteLine("");
+            pioner.Print();
+            Console.ReadLine();
         }
     }
-    public class Vehicle
-    {
 
+    // calls the vehicles to make a list for the driver
+     class CarLot
+    {
+        //names the lot 
+        public string Names;
+        //makes the list of vehicles in the lot        
+        public List<Vehicle> vehicles;
+        public CarLot(string name)
+        {
+            Names = name;
+            vehicles = new List<Vehicle>();
+        }
+        public CarLot(string name, List<Vehicle> list)
+        {
+            vehicles = new List<Vehicle>();
+            Names = name;
+            foreach (Vehicle v in list)
+                vehicles.Add(v);
+        }
+        public void list(Vehicle v)
+        {
+            vehicles.Add(v);
+        }
+        public void Print()
+        {
+            Console.Write($"{Names}\n" );
+            int items = 1;
+            foreach (Vehicle v in vehicles)
+            {
+                Console.Write($"{items}. ");
+                v.stats();
+                items++;
+            }
+        }
     }
 }
+    
+
+    //makes the vehicle classes
+     public abstract class Vehicle
+    {
+        // finds the liscene number of the vehicle
+     public string LicenseNumber;
+        //finds the make 
+     public string Make;
+        //finds the model
+     public string Model;
+        //finds the price 
+     public double Price;
+        public Vehicle(string license, string make, string model, double price)
+        {
+        LicenseNumber = license;
+        Make = make;
+        Model = model;
+        Price = price;
+        }
+
+     public virtual void stats()
+        {
+        Console.Write($"License: {LicenseNumber}   Model: {Model}   Make: {Make}   Price: {Price}");
+        }
+    }
+    //car sub class
+     public class Car : Vehicle
+    {
+        // type of car ie sedan coupe
+        public string Type;
+        //how many doors does the car have
+        public string NumberDoors;
+    public override void stats()
+    {
+        Console.Write($"License: {LicenseNumber}   Model: {Model}   Make: {Make}   Type: {Type}   Doors: {NumberDoors}   Price: {Price}");
+    }
+}
+    // truck sub class
+     public class Truck : Vehicle
+    {
+        // truck bedsize ie full exstended 
+        public string BedSize;
+     public override void stats()
+     {
+        Console.Write($"License: {LicenseNumber}   Model: {Model}   Make: {Make}   Bed size: {BedSize}   Price: {Price}");
+     }
+}
+
+
+
--- point/Program.cs
+++ point/Program.cs
@@ -6,31 +6,128 @@ using System.Threading.Tasks;
 
 namespace point
 {
-    class Program
-    {
-        static void Main(string[] args)
+   
+    public class Point2D
+    {   int X;
+        int Y;
+        public Point2D()
         {
-            Point2D p = new Point2D();
-            p.X = 1;
+            X = 0;
+            Y = 0;
+        }
+         
+        public Point2D(int x, int y)
+        {
+             x = X;
+             y = Y;
+           
+
+        }
+        public override bool Equals(object o)
+        {
+            if (o.GetType().Equals(this.GetType()))
+            //if (o is Point2D)
+            {
+                Point2D point = (Point2D)o;
+                return X == point.X && Y == point.Y;
+            }
+            else
+            {
+                return false;
+            }
+        }
+            public override string ToString()
+            {
+            return "(" + X + "," + Y + ")";
+            //new way return $"({X} , {Y})";
+            }
+        
+
+        
+    }
+
+    public class Point3D : Point2D
+    {
+        public Point3D()
+        {
+            int Z;
+            Z = 0;
+        }
+        public Point3D(int x, int y, int z) : base(x, y)
+        {
+            Z = z;
+        }
+
+        // your code here
+    }
+
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            Point2D p1 = new Point2D();
+            Point2D p2 = new Point2D(7, 5);
+            Point2D p3 = new Point2D(-2, 4);
+            Point2D p4 = new Point2D(7, 5);
+
+            Console.WriteLine("p1 = {0}", p1);
+            Console.WriteLine("p2 = {0}", p2);
+            Console.WriteLine("p3 = {0}", p3);
+            Console.WriteLine("p4 = {0}", p4);
+
+            Console.WriteLine("p2 == p3? {0}", p2 == p3);
+            Console.WriteLine("p2.Equals(p3)? {0}", p2.Equals(p3));
+            Console.WriteLine("p2 == p4? {0}", p2 == p4);
+            Console.WriteLine("p2.Equals(p4)? {0}", p2.Equals(p4));
+
+            Console.WriteLine("========");
+
+            Point3D p5 = new Point3D();
+            Point3D p6 = new Point3D(7, 5, 12);
+            Point3D p7 = new Point3D(-2, 4, -4);
+            Point3D p8 = new Point3D(7, 5, 12);
+
+            Console.WriteLine("p5 = {0}", p5);
+            Console.WriteLine("p6 = {0}", p6);
+            Console.WriteLine("p7 = {0}", p7);
+            Console.WriteLine("p8 = {0}", p8);
+
+            Console.WriteLine("p6 == p7? {0}", p6 == p7);
+            Console.WriteLine("p6.Equals(p7)? {0}", p6.Equals(p7));
+            Console.WriteLine("p6 == p8? {0}", p6 == p8);
+            Console.WriteLine("p6.Equals(p8)? {0}", p6.Equals(p8));
+
+            Console.WriteLine("========");
         }
     }
-    class point2D
-    {
-        public int X { get; set; }
+    /*class point2D
+   {
+       public int X { get; set; }
 
-       /* private int _x;
+      private int _x;
 
-        public int X
-        {
-            get
-            {
-                return _x;
+       public int X
+       {
+           get
+           {
+               return _x;
 
-            }
-            set
-            {
-                _x = value;*/
-            }
-        }
-    }
+           }
+           set
+           {
+               _x = value;*/
 }
+    /* basic math
+     static int add(int x, int y)
+  {
+  return x + y;
+ }  
+ harder math func to match week 5 1 whiteboard
+ static Func<Int, int> add(int x){
+ return y => x + y;
+ }
+     
+     */
+        
+    
+
